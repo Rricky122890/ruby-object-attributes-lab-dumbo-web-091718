dcfdbc1608d3a7806b7f0318a0eb27885e130c9f
Ricardo Jean-Pierre <rjeanpierre90@gmail.com>
2018-09-19 23:00:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,49 +1,19 @@
-# class Dog
-#   attr_accessor :name
-  
-#   def initialize (name)
-#   @name = name 
-#   end 
-  
-#   # def name
-#   #   @name 
-#   # end
-  
-#   # def name=(new_name)
-#   #   @new_name = new_name 
-#   # end
-  
-# end
-
-# fido = Dog.new("Fido")
-# puts fido.name
-
 class Dog
-  def initialize(dog_name, breed)
-    @dog_name = dog_name
-    @this_dogs_breed= breed
-  end
-   def name=(dog_name)
-    @this_dogs_name = dog_name
-  end
-   def name
-    @this_dogs_name
-  end
-   def breed=(dog_breed)
-    @this_dogs_breed = dog_breed
-  end
-   def breed
-    @this_dogs_breed
-  end
-   def bark
-    puts "Woof!"
-  end
-   def sit
-    puts "The Dog is sitting"
-  end
+  attr_accessor :name
+  
+  def initialize (name)
+   @name = name 
+  end 
+  
+  # def name
+  #   @name 
+  # end
+  
+  # def name=(new_name)
+  #   @new_name = new_name 
+  # end
+  
 end
- fido = Dog.new("fido", "beagle")
-# snoopy = Dog.new
-# lassie = Dog.new
-#
-# lassie.name = "Lassie"
+
+fido = Dog.new("Fido")
+puts fido.name
